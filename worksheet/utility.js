4473bff9fe3be550778c3f3023a32e69c5393fbd
Lily Cholmondeley <lily.cholmo1@gmail.com>
2024-02-15 12:23:39 +0000

    Completed unit tests

--- worksheet/utility.js
+++ worksheet/utility.js
@@ -11,6 +11,21 @@ export function subtract(a, b) {
  * "export" to the function definition (as seen in the
  * subtract example above)
  */
-function add(a, b) {
+export function add(a, b) {
   return a + b;
+}
+
+//Create a function called compare that accempts two arrays of numbers and compares the contents.
+export function compare(arr1, arr2) {
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
+}
+
+//Create and export a function called largest that accepts an array of numbers and returns the largest.
+export function largest(arr) {
+  return Math.max(...arr);
+}
+
+//Create and export a function called zeroest that accepts an array of integer numbers and returns the one closest to zero
+export function zeroest(arr) {
+  return arr.reduce((a, b) => Math.abs(b) < Math.abs(a) ? b : a);
 }
